64e6c5a45786745aee765c8c0590ce3d56549722
Pablo <pablo144@gmail.com>
2017-03-22 00:17:58 -0500

    Questao 2 pronta

--- app/src/main/java/com/example/laboratorio/ciclodevida/Exercicio2Tela1.java
+++ app/src/main/java/com/example/laboratorio/ciclodevida/Exercicio2Tela1.java
@@ -1,14 +1,11 @@
 package com.example.laboratorio.ciclodevida;
 
 import android.content.Intent;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
-/**
- * Created by Pablo on 21/03/2017.
- */
 public class Exercicio2Tela1 extends AppCompatActivity {
     EditText nome;
 
@@ -16,22 +13,22 @@ public class Exercicio2Tela1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exercicio2_tela1);
-        nome = (EditText) findViewById(R.id.edNome);
+        nome = (EditText) findViewById(R.id.edPalavra);
     }
 
+    public void enviarResultado(View view) {
+        String n = String.valueOf(nome.toString());
 
-    //public void enviarResposta(View view) {
+        String msg = null;
 
-      //  String n = String.valueOf(nome.toString());
-//       String msg = "A primeira letra e: " + n.charAt(0)+ "Ultima letra e: " + n.charAt(nome.length()-1);
+        if ((nome.length() < 5)) {
+            msg += "Informe uma palavra maior que 5 letras";
+        }else msg+= "A primeira letra e: " + n.charAt(0)+ " Ultima letra e: " + n.charAt(nome.length()-1);
 
-  //      if((nome.length() < 5))
-    //        msg += "Informe uma palavra maior que 5 letras";
-
-//        Intent i = new Intent(this, Exercicio2Tela2.class);
-       // i.putExtra("resultado", msg);
-        //startActivity(i);
-
-
+        Intent i = new Intent(this, Exercicio2Tela2.class);
+        i.putExtra("resultado", msg);
+        startActivity(i);
 
     }
+
+}
--- app/src/main/java/com/example/laboratorio/ciclodevida/Exercicio3Tela1.java
+++ app/src/main/java/com/example/laboratorio/ciclodevida/Exercicio3Tela1.java
@@ -1,18 +1,19 @@
 package com.example.laboratorio.ciclodevida;
 
-import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
-/**
- * Created by Pablo on 21/03/2017.
- */
-public class Exercicio3Tela1 extends AppCompatActivity{
-
+public class Exercicio3Tela1 extends AppCompatActivity {
+    EditText nome;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exercicio3_tela1);
+        nome = (EditText) findViewById(R.id.edPalavra);
+    }
+    public void enviarResposta(View view){
 
     }
 }
